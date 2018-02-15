1c8271589975818d04ba118f7232cf7287105aff
mttstt <mtt_stt@yahoo.it>
2018-02-15 13:28:59 +0100

    Update send.py

--- send.py
+++ send.py
@@ -8,28 +8,41 @@ import time
 import pigpio
 
 Pulse_len=360
-segnal = "110011000000100100000000000000001011100100000001101000100000000000"
-GP=(1<<22) # selects gpio 17
+
+1up =   ""
+1stop = ""
+1down = "
+2up =   ""
+2stop = ""
+2down = "
+3up =   ""
+3stop = ""
+3down = "
+4up =   ""
+4stop = ""
+4down = "
+5up =   ""
+5stop = ""
+5down = "
+6up =   "110011000000100100000000000000001011100100000001101000100000000000"
+6stop = ""
+6down = "
+7up =   ""
+7stop = ""
+7down = "
+8up =   ""
+8stop = ""
+8down = "
+9up =   ""
+9stop = ""
+9down = "
+
+GPIO = 22 # selects gpio
+GP=(1<<GPIO) 
 NONE=0       # selects no gpios
-
-pi = pigpio.pi() # connect to Pi
-if not pi.connected:
-   exit()
-
-pi.set_mode(22, pigpio.OUTPUT)
 f1=[]
 
-args = len(sys.argv)
-
-if args > 1:
-      tx=_433.tx(pi, gpio=TX)
-      for i in range(args-1):
-         print("sending {}".format(sys.argv[i+1]))
-         tx.send(int(sys.argv[i+1]))
-         time.sleep(1)
-
 def transmit_code(code):         
-   
    # ------ Preamble ----------------------------------------
    f1.append(pigpio.pulse(NONE, NONE, 3000)) # added 3 millis
    for i in range(1,12):
@@ -43,12 +56,13 @@ def transmit_code(code):
       if c == '1':
          f1.append(pigpio.pulse(GP, NONE, Pulse_len*1))
          f1.append(pigpio.pulse(NONE, GP, Pulse_len*2))
-      if c == '0':
+      elif c == '0':
          f1.append(pigpio.pulse(GP, NONE, Pulse_len*2))
          f1.append(pigpio.pulse(NONE, GP, Pulse_len*1))
+      else:
+         continue
    f1.append(pigpio.pulse(NONE, NONE, 3000)) # added 3 millis
    # -------End Segnal ------------------------------------------
-
    pi.wave_clear()
    pi.wave_add_generic(f1)
    f = pi.wave_create() # create and save id
@@ -56,6 +70,17 @@ def transmit_code(code):
       pi.wave_send_once(f)
    #pi.wave_send_repeat(f)
    #time.sleep(0.3)
+
+pi = pigpio.pi() # connect to Pi
+if not pi.connected:
+   exit()
+   
+pi.set_mode(GPIO, pigpio.OUTPUT)
+
+if __name__ == '__main__':
+    for argument in sys.argv[1:]:
+        print("sending {}".format(argument))   
+        exec('transmit_code(' + str(argument) + ')')
    
 pi.wave_tx_stop()
 pi.stop()
