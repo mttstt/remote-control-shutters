234be3fddcfd2dba6c656a12687e4f85a286dfda
mttstt <mtt_stt@yahoo.it>
2018-12-29 21:27:54 +0100

    Update shutter.js

--- shutter.js
+++ shutter.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var http = require('http');
 var url = require('url');
 var exec = require('child_process').exec;
@@ -9,22 +10,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   console.log('=>',q);
-  if ((q.shutter != undefined) && (q.shutter == 'upG')){
-        var shutters = ['up6','up7','up8','up9'];
-        shutters.forEach(function(s) {
-                var command = '/home/pi/send.py '+ s;
-                console.log('command',command);
-                exec(command, function callback(error, stdout, stderr){
-                        console.log('stdout: ' + stdout);
-                        console.log('stderr: ' + stderr);
-                        if (error !== null) {
-                                console.log('exec error: ' + error);
-                        }
-                });
-        res.write(s);
-        });
-  };
- if ((q.shutter != undefined) && (q.shutter != 'upG')) {
+
+ if (q.shutter != undefined) {
         var command = '/home/pi/send.py '+ q.shutter;
         console.log('command',command);
         exec(command, function callback(error, stdout, stderr){
@@ -34,8 +21,9 @@ http.createServer(function (req, res) {
                         console.log('exec error: ' + error);
                 }
         });
-        res.write(q.shutter);
-}  
-  
-res.end();
+        res.write(command);
+
+}
+
+  res.end();
 }).listen(8889);
