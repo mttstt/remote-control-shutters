23a1d4bbb3fe91ff950f188f4955c137a1b226b8
mttstt <mtt_stt@yahoo.it>
2018-12-29 15:38:26 +0100

    Update shutter.js

--- shutter.js
+++ shutter.js
@@ -4,15 +4,23 @@ var http = require('http');
 var url = require('url');
 var exec = require('child_process').exec;
 
+console.log("Http server starting...");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
+  console.log('=>',q);
   var command = '/home/pi/send.py '+ q.shutter;
-  exec(command, function callback(error, stdout, stderr){
-        console.log(stdout);
-  });
-
+  if (q.shutter != undefined) {
+      console.log('command',command);
+      exec(command, function callback(error, stdout, stderr){
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+      });
+  }
   res.write(command);
   res.end();
 }).listen(8889);
