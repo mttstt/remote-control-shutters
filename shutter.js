0da6a5b928ee7ae11d2643f4908cc50150bd624f
mttstt <mtt_stt@yahoo.it>
2018-12-31 15:41:40 +0100

    Update shutter.js

--- shutter.js
+++ shutter.js
@@ -10,20 +10,72 @@ console.log("Http server starting...");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  console.log('=>',q);
 
-  if (q.shutter != undefined) {
-        var command = '/home/pi/send.py '+ q.shutter;
-        console.log('command',command);
-        exec(command, function callback(error, stdout, stderr){
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                if (error !== null) {
-                        console.log('exec error: ' + error);
-                }
-        });
-        res.write(command);
-  }
+  var q = url.parse(req.url, true).query;
+
+ if (q.shutter != undefined) {
+  var qshutter = q.shutter;
+
+  //console.log('shutter: ',q' where: ',q.where);
+
+  if ((q.where == 'up') || (q.where == 'do') || (q.where == 'st')) {
+        var id;
+        //switch (q.shutter.split(" ").splice(-1)[0]){
+
+//switch (q.shutter.trim()){
+
+switch (true){
+
+        case /cameretta/.test(q.shutter):
+                id="1";
+                break;
+        case (/camera/.test(q.shutter) &&/nonna/.test(q.shutter) ):
+                id="2";
+                break;
+        case /camera grande/.test(q.shutter):
+                id="3";
+                break;
+        case /camera matrimoniale/.test(q.shutter):
+                id="4";
+                break;
+        case (/bagno/.test(q.shutter) && /piccolo/.test(q.shutter) ):
+                id="5";
+                break;
+        case (/sala/.test(q.shutter) && /piccola/.test(q.shutter) ):
+                id="6";
+                break;
+        case (/sala/.test(q.shutter) && /grande/.test(q.shutter) ):
+                id="7";
+                break;
+        case /ripostiglio/.test(q.shutter):
+                id="8";
+                break;
+        case /cucina/.test(q.shutter):
+                id="9"
+                break;
+        case /sala/.test(q.shutter):
+                id="6&7"
+                break;
+        default:
+                console.log("Error in input: "+q.shutter+" => "+(q.shutter.split(" ").splice(-1)));
+        }
+
+        qshutter=q.where+id;
+        if (id="6&7") {qshutter = q.where+"6 "+q.where+"7" }
+   }
+
+   console.log('command: ',qshutter);
+
+   var command = '/home/pi/send.py '+ qshutter;
+   //console.log('command',command);
+   exec(command, function callback(error, stdout, stderr){
+     console.log('stdout: ' + stdout);
+     console.log('stderr: ' + stderr);
+        if (error !== null) {
+                console.log('exec error: ' + error);
+        }
+   });
+   res.write(command);
+ }
  res.end();
 }).listen(8889);
