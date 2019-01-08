c05bce34e0945d8b8a1765cd3317ce88ccfdd808
mttstt <mtt_stt@yahoo.it>
2019-01-08 23:56:13 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,33 +8,55 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {dialogflow} = require('actions-on-google');
-
 const app = dialogflow({debug: true});
-
-const map = { "sala": "192.168.1.1", "cucina": "192.168.1.113", "camera matrimoniale": "192.168.1.113"};
+var http = require('http');
+const map = { "sala": "192.168.1.112", "cucina": "192.168.1.113", "camera matrimoniale": "192.168.1.113"};
 
 const temperatura = () => {
-  var ip = '192.168.1.112';
-  try { var response = HTTP.get("http://"+ip+"/json", {});     
-      if (response.data !== null) {
-                var data = [response.data.Sensors[0].Temperature, response.data.Sensors[0].Humidity];
-                return data;
-             } else {console.log('Warning response.data null:',ip); }
-	}
-  catch(e){ console.log('Warning reading sensor:',ip);}
-};
+	var ip = '192.168.1.112';
+	http.get("http://"+ip+"/json", (res) => {
+	  const { statusCode } = res;
+	  const contentType = res.headers['content-type'];
+	  let error;
+	  if (statusCode !== 200) {
+	    error = new Error('Request Failed.\n' +
+			      `Status Code: ${statusCode}`);
+	  } else if (!/^application\/json/.test(contentType)) {
+	    error = new Error('Invalid content-type.\n' + `Expected application/json but received ${contentType}`);
+	  }
+	  if (error) {
+	    console.error(error.message);
+	    res.resume();
+	    return;
+	  }
+	  res.setEncoding('utf8');
+	  let rawData = '';
+	  res.on('data', (chunk) => { rawData += chunk; });
+	  res.on('end', () => {
+	    try {
+	      const parsedData = JSON.parse(rawData);
+	      var data = [parsedData.Sensors[0].Temperature, parsedData.Sensors[0].Humidity];
+	      console.log('====> ',data);
+	      return data;
+	    } catch (e) {
+	      console.error(e.message);
+	    }
+	  });
+	  }).on('error', (e) => { console.error(`Got error: ${e.message}`);});
+}
+
 
 app.intent('Ciao Mora', conv => {
   conv.close(`Mora viva e vegeta!`)
 })
 
-app.intent('temperature', async (conv,{stanza}) => {
-  conv.ask(`Connessione mora ok.`)
-  const data = await temperatura();
-  conv.close(`La tempertura in ${name} è pari a ${data[0]} e l'umidità è pari al ${data[1]} percento.`);
-});
 
+app.intent('dimmi la temperatura in camera', (conv,{stanza}) => {
+  return temperatura().then((data) => {
+    conv.close(`La tempertura in ${stanza} è pari a ${data} e l'umidità è pari al ${data} percento.`);
+});
 
 const expressApp = express().use(bodyParser.json());
 expressApp.post('/fulfillment', app);
-expressApp.listen(7000);     
+expressApp.listen(7000);
+
