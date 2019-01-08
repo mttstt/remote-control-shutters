c22458b9e891823ada8b3f91568edf26ab26a4c0
mttstt <mtt_stt@yahoo.it>
2019-01-08 15:48:44 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,33 +5,27 @@
 
 'use strict';
 
-const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const {
-  dialogflow,
-} = require('actions-on-google');
-
-const app = dialogflow({
-  debug: true,
-});
-
+const {dialogflow} = require('actions-on-google');
+const app = dialogflow({debug: true});
 const temperatura = () => {
-  // ...
+  return '25';
 };
-
 const umidita = () => {
-  // ...
+  return '80';
 };
-
-
 app.intent('Ciao Mora', conv => {
   conv.close('Ciao mora presente!')
 })
-
 app.intent('temperature', async (conv,{stanza}) => {
   const temp = await temperatura();
   const humi = await umidita();
   conv.close(`La tempertura in ${name} è pari a ${temp} e l'umidità è pari al ${humi} percento!`);
 });
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+
+const expressApp = express().use(bodyParser.json());
+expressApp.post('/fulfillment', app);
+expressApp.listen(3000);
