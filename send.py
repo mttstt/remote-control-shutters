76174e02d33cb15ae7927d4a90038fc417aef341
mttstt <mtt_stt@yahoo.it>
2019-01-31 18:55:37 +0100

    Update send.py

--- send.py
+++ send.py
@@ -1,128 +1,261 @@
-#!/usr/bin/env python
-# send.py 2018-02-15
-# Public Domain
+// Copyright 2019, mtt stt, Inc.
 
-import time
-import pigpio
-import sys
+// References:
+// https://1technophile.blogspot.com/2016/08/low-cost-low-power-6ua-garden-433mhz.html
+// https://github.com/esp8266/Arduino/blob/master/doc/reference.rst
+// https://arduino.stackexchange.com/questions/44531/arduino-esp8266-direct-fast-control-of-the-digital-pins
+// https://www.instructables.com/id/Using-an-ESP8266-to-Control-Mains-Sockets-Using-43/
+// http://nerdralph.blogspot.com/2015/04/a-4mbps-shiftout-for-esp8266arduino.html
 
-Pulse_len=360
-GPIO = 22 # selects gpio
-GP=(1<<22)
-NONE=0 # selects no gpios
-NUM_ATTEMPTS = 3
-
-# ------- gate ---------
-short_delay = 760
-long_delay = 1520
-extended_delay = 0.5
-canc = '01111010010000'
-# ----------------------
+// Note(1): Watt OK. max pinout watt of Nodemcu 1.0 10mW, Cheap transmitter 433mhz 10 10mW
+// Note(2): The segnal of blinks is not rcswitch compliant, so I have write a program without this library
+// Note(3): digitalwrite() Esp8286 function runs to 160Khz (6,25 μs): it is enough for this program
 
 
-up0 = '110011000000100100000000000000000001100101010001101000100000000000'
-st0 = '110011000000011000000000000000000001100101010001101000100000000000'
-do0 = '110011000000001000000000000000000001100101010001101000100000000000'
-up1 = '?'
-st1 = '?'
-do1 = '?'
-up2 = '110011000000100100000000000000001001100100010001101000100000000000'
-st2 = '110011000000011000000000000000001001100100010001101000100000000000'
-do2 = '110011000000001000000000000000001001100100010001101000100000000000'
-up3 = '110011000000100100000000000000000101100101100001101000100000000000'
-st3 = '110011000000011000000000000000000101100101100001101000100000000000'
-do3 = '110011000000001000000000000000000101100101100001101000100000000000'
-up4 = '110011000000100100000000000000001101100100100001101000100000000000'
-st4 = '110011000000011000000000000000001101100100100001101000100000000000'
-do4 = '110011000000001000000000000000001101100100100001101000100000000000'
-up5 = '110011000000100100000000000000000011100101000001101000100000000000'
-st5 = '110011000000011000000000000000000011100101000001101000100000000000'
-do5 = '110011000000001000000000000000000011100101000001101000100000000000'
-up6 = '110011000000100100000000000000001011100100000001101000100000000000'
-st6 = '110011000000011000000000000000001011100100000001101000100000000000'
-do6 = '110011000000001000000000000000001011100100000001101000100000000000'
-up7 = '110011000000100100000000000000000111100101111110101000100000000000'
-st7 = '110011000000011000000000000000000111100101111110101000100000000000'
-do7 = '110011000000001000000000000000000111100101111110101000100000000000'
-up8 = '110011000000100100000000000000001111100100111110101000100000000000'
-st8 = '110011000000011000000000000000001111100100111110101000100000000000'
-do8 = '110011000000001000000000000000001111100100111110101000100000000000'
-up9 = '110011000000100100000000000000000000010101011110101000100000000000'
-st9 = '110011000000011000000000000000000000010101011110101000100000000000'
-do9 = '110011000000001000000000000000000000010101011110101000100000000000'
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>   // Include the WebServer library
 
-up88 ='010101010000100100000000000000001111100100111110101000100000000000'
-st88 ='010101010000011000000000000000001111100100111110101000100000000000'
-do88 ='010101010000001000000000000000001111100100111110101000100000000000'
-up99 ='010101010000100100000000000000000000010101011110101000100000000000'
-st99 ='010101010000011000000000000000000000010101011110101000100000000000'
-do99 ='010101010000001000000000000000000000010101011110101000100000000000'
+#define SERVER_PORT 7000
+const int pulse = 360; //μs
 
-def transmit_code(code):
-      f1=[]
-      # ------ Preamble ----------------------------------------
-      f1.append(pigpio.pulse(NONE, NONE, 3000)) # added 3 millis
-      for i in range(0,12):
-         f1.append(pigpio.pulse(GP, NONE, Pulse_len*1))
-         f1.append(pigpio.pulse(NONE, GP, Pulse_len*1))
-      # ------ End Preamble ------------------------------------
+//# ------- gate ---------
+const int short_delay = 760; //μs
+const int long_delay = 1520;
+const int extended_delay = 0.5;
+String canc = "01111010010000";
+//# ----------------------
 
-      # -------Segnal ------------------------------------------
-      f1.append(pigpio.pulse(NONE, NONE, 3500)) # added 3,5 millis
-      for c in code:
-         if c == '1':
-            f1.append(pigpio.pulse(GP, NONE, Pulse_len*1))
-            f1.append(pigpio.pulse(NONE, GP, Pulse_len*2))
-         elif c == '0':
-            f1.append(pigpio.pulse(GP, NONE, Pulse_len*2))
-            f1.append(pigpio.pulse(NONE, GP, Pulse_len*1))
-         else:
-            continue
-      f1.append(pigpio.pulse(NONE, NONE, 3000)) # added 3 millis
-      # -------End Segnal ------------------------------------------
-      pi.wave_clear()
-      pi.wave_add_generic(f1)
-      f = pi.wave_create() # create and save id
-      for t in range(NUM_ATTEMPTS):
-         print("sending {}".format(t))
-         pi.wave_send_repeat(f)
-         time.sleep(0.2)
+String up0 = "110011000000100100000000000000000001100101010001101000100000000000";
+String st0 = "110011000000011000000000000000000001100101010001101000100000000000";
+String do0 = "110011000000001000000000000000000001100101010001101000100000000000";
+String up1 = "?";
+String st1 = "?";
+String do1 = "?";
+String up2 = "110011000000100100000000000000001001100100010001101000100000000000";
+String st2 = "110011000000011000000000000000001001100100010001101000100000000000";
+String do2 = "110011000000001000000000000000001001100100010001101000100000000000";
+String up3 = "110011000000100100000000000000000101100101100001101000100000000000";
+String st3 = "110011000000011000000000000000000101100101100001101000100000000000";
+String do3 = "110011000000001000000000000000000101100101100001101000100000000000";
+String up4 = "110011000000100100000000000000001101100100100001101000100000000000";
+String st4 = "110011000000011000000000000000001101100100100001101000100000000000";
+String do4 = "110011000000001000000000000000001101100100100001101000100000000000";
+String up5 = "110011000000100100000000000000000011100101000001101000100000000000";
+String st5 = "110011000000011000000000000000000011100101000001101000100000000000";
+String do5 = "110011000000001000000000000000000011100101000001101000100000000000";
+String up6 = "110011000000100100000000000000001011100100000001101000100000000000";
+String st6 = "110011000000011000000000000000001011100100000001101000100000000000";
+String do6 = "110011000000001000000000000000001011100100000001101000100000000000";
+String up7 = "110011000000100100000000000000000111100101111110101000100000000000";
+String st7 = "110011000000011000000000000000000111100101111110101000100000000000";
+String do7 = "110011000000001000000000000000000111100101111110101000100000000000";
+String up8 = "110011000000100100000000000000001111100100111110101000100000000000";
+String st8 = "110011000000011000000000000000001111100100111110101000100000000000";
+String do8 = "110011000000001000000000000000001111100100111110101000100000000000";
+String up9 = "110011000000100100000000000000000000010101011110101000100000000000";
+String st9 = "110011000000011000000000000000000000010101011110101000100000000000";
+String do9 = "110011000000001000000000000000000000010101011110101000100000000000";
+String up88 ="010101010000100100000000000000001111100100111110101000100000000000";
+String st88 ="010101010000011000000000000000001111100100111110101000100000000000";
+String do88 ="010101010000001000000000000000001111100100111110101000100000000000";
+String up99 ="010101010000100100000000000000000000010101011110101000100000000000";
+String st99 ="010101010000011000000000000000000000010101011110101000100000000000";
+String do99 ="010101010000001000000000000000000000010101011110101000100000000000";
+
+#define pin D2  //GPIO4
+#define NUM_ATTEMPTS 3
+#define TRACE 1  // 0= trace off 1 = trace on Do we want to see trace for debugging purposes
+void trc(String msg);              // function prototypes 
+void transmit_code(String code);
+void shuttercommand();
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+ESP8266WebServer server(SERVER_PORT);    // Create a webserver object that listens for HTTP request on port 80
 
 
-def transmit_code_canc(code):
-      f1=[]
-      # -------Segnal ------------------------------------------
-      f1.append(pigpio.pulse(NONE, NONE, 3500)) # added 3,5 millis
-      for c in code:
-         if c == '1':
-            f1.append(pigpio.pulse(GP, NONE, short_delay))
-            f1.append(pigpio.pulse(NONE, GP, long_delay))
-         elif c == '0':
-            f1.append(pigpio.pulse(GP, NONE, long_delay))
-            f1.append(pigpio.pulse(NONE, GP, short_delay))
-         else:
-            continue
-      f1.append(pigpio.pulse(NONE, NONE, 3000)) # added 3 millis
-      # -------End Segnal ------------------------------------------
-      pi.wave_clear()
-      pi.wave_add_generic(f1)
-      f = pi.wave_create() # create and save id
-      for t in range(NUM_ATTEMPTS):
-         print("sending {}".format(t))
-         pi.wave_send_repeat(f)
-         time.sleep(extended_delay)
+void setup(void){
+  pinMode(pin,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);   
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  wifiMulti.addAP("MTT_2.4", "xxx");   // add Wi-Fi networks you want to connect to
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
 
-pi = pigpio.pi() # connect to Pi
-if not pi.connected:
-   exit()
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  
+  server.on("/", HTTP_GET, []() { server.send(200, "text/html", "<h1> GatewayRF </h1> <p>"+server.uri()+" Web Serber ready !"); });
+  server.on("/gate", HTTP_GET, []() { server.send(200, "text/html", "<h1> GatewayRF </h1> <p>"+server.uri()+"</p>");
+                                      transmit_gate_code(canc); 
+                                    });  
+  server.on("/shutter", HTTP_GET, []() { shuttercommand(); });  
+  server.onNotFound([]() { server.send(404, "text/plain", "404: Not Found"); });
+  
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started"); 
+}
 
-pi.set_mode(GPIO, pigpio.OUTPUT)
-if __name__ == '__main__':
-    for argument in sys.argv[1:]:
-        if argument == 'canc':
-                exec('transmit_code_canc(' + str(argument) + ')')
-        else:
-                exec('transmit_code(' + str(argument) + ')')
 
-pi.wave_tx_stop()
-pi.stop()
+void loop(void){ 
+  server.handleClient();            // Listen for HTTP requests from clients    
+}
+
+// trace function
+void trc(String msg){if (TRACE) { Serial.println(msg); } }
+
+
+void transmit_code(String code){
+  int len = code.length();
+  trc(code);
+  for (int i = 0; i < NUM_ATTEMPTS; i++) 
+  {        
+      // ----------------------- Preamble ----------------------
+      digitalWrite(LED_BUILTIN, LOW);
+      //digitalWrite(pin, LOW);
+      //delay(50);  // sleep for 0,3 seconds
+      for (int i = 0; i < 12; ++i) 
+      {
+        digitalWrite(pin, HIGH); 
+        delayMicroseconds(pulse);
+        digitalWrite(pin, LOW); 
+        delayMicroseconds(pulse);  
+      }
+      // ---------------------- End Preamble --------------------
+      // -----------------------Segnal --------------------------
+      //trc("transmit segnal");
+      digitalWrite(pin, LOW);
+      delayMicroseconds(3500); // added 3,5 millis
+      for (int j=0;j<len;++j) 
+      {
+         char ch = code.charAt(j);        
+         if (ch == '1')
+         {
+           //trc("1");
+           digitalWrite(pin, HIGH);         
+           delayMicroseconds(pulse);
+           digitalWrite(pin, LOW);          
+           delayMicroseconds(pulse*2);
+         } 
+         else 
+         { 
+           //trc("0");
+           digitalWrite(pin, HIGH); 
+           delayMicroseconds(pulse*2);
+           digitalWrite(pin, LOW); 
+           delayMicroseconds(pulse);
+         }
+      }
+      digitalWrite(pin, LOW);
+      delayMicroseconds(50
+      00); // added 2 millis
+      digitalWrite(LED_BUILTIN, LOW);         
+    // ---------------------End Segnal --------------------------   
+    }
+    trc("Segnal sended ");
+}
+
+
+void transmit_gate_code(String code){
+  int len = code.length();
+  trc(code);
+  for (int i = 0; i < NUM_ATTEMPTS; i++) 
+  {              
+      digitalWrite(pin, LOW);
+      for (int j=0;j<len;++j) 
+      {
+         char ch = code.charAt(j);        
+         if (ch == '1')
+         {
+           digitalWrite(pin, HIGH);         
+           delayMicroseconds(short_delay);
+           digitalWrite(pin, LOW);          
+           delayMicroseconds(long_delay);
+         } 
+         else 
+         {            
+           digitalWrite(pin, HIGH); 
+           delayMicroseconds(long_delay);
+           digitalWrite(pin, LOW); 
+           delayMicroseconds(short_delay);
+         }
+      }
+      digitalWrite(pin, LOW);
+      delayMicroseconds(extended_delay); // added 2 millis
+      delay(extended_delay);
+      digitalWrite(LED_BUILTIN, LOW);         
+    }
+    trc("Segnal sended ");
+}
+
+
+//Examples:
+// 
+//  curl http://XXX.dlinkddns.com:7000/shutter/?s=up6&s=up7&s=up8&s=up9
+//
+void shuttercommand() { 
+  String message = "<h1> Gateway Rf </h1>\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+    if (server.argName(i) == "s") {
+        String ch;      
+        if ( server.arg (i) == "up0" ) { ch = up0; }
+        else if ( server.arg (i) == "st0") { ch = st0; }
+        else if ( server.arg(i) == "do0" ) { ch = do0; }      
+        else if ( server.arg(i) == "up1" ) { ch = up1; }
+        else if ( server.arg(i) == "st1" ) { ch = st1; }
+        else if ( server.arg(i) == "do1" ) { ch = do1; }      
+        else if ( server.arg(i) == "up2" ) { ch = up2; }
+        else if ( server.arg(i) == "st2" ) { ch = st2; }
+        else if ( server.arg(i) == "do2" ) { ch = do2; }      
+        else if ( server.arg(i) == "up3" ) { ch = up3; }
+        else if ( server.arg(i) == "st3" ) { ch = st3; }
+        else if ( server.arg(i) == "do3" ) { ch = do3; }      
+        else if ( server.arg(i) == "up4" ) { ch = up4; }
+        else if ( server.arg(i) == "st4" ) { ch = st4; }
+        else if ( server.arg(i) == "do4" ) { ch = do4; }      
+        else if ( server.arg(i) == "up5" ) { ch = up5; }
+        else if ( server.arg(i) == "st5" ) { ch = st5; }
+        else if ( server.arg(i) == "do5" ) { ch = do5; }     
+        else if ( server.arg(i) == "up6" ) { ch = up6; }
+        else if ( server.arg(i) == "st6" ) { ch = st6; }
+        else if ( server.arg(i) == "do6" ) { ch = do6; }      
+        else if ( server.arg(i) == "up7" ) { ch = up7; }
+        else if ( server.arg(i) == "st7" ) { ch = st7; }
+        else if ( server.arg(i) == "do7" ) { ch = do7; }      
+        else if ( server.arg(i) == "up8" ) { ch = up8; }
+        else if ( server.arg(i) == "st8" ) { ch = st8; }
+        else if ( server.arg(i) == "do8" ) { ch = do8; }      
+        else if ( server.arg(i) == "up9" ) { ch = up9; }
+        else if ( server.arg(i) == "st9" ) { ch = st9; }
+        else if ( server.arg(i) == "do9" ) { ch = do9; }
+        else {Serial.print("Input error\n");  message += "\nError"; }      
+        transmit_gate_code(ch);
+    } 
+  }  
+  server.send ( 200, "text/plain", message );
+}
