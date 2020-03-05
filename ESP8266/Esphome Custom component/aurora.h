4d4ec5b3cf47df1c3a843a9880d4724abef9bd5b
mttstt <mtt_stt@yahoo.it>
2020-03-05 09:49:14 +0100

    Update aurora.h

--- ESP8266/Esphome Custom component/aurora.h
+++ ESP8266/Esphome Custom component/aurora.h
@@ -12,30 +12,53 @@ class MyCustomSensor : public PollingComponent {
 
   Sensor *DspValue_sensor = new Sensor();
   Sensor *ReadCumulatedEnergy_sensor = new Sensor();
+  Sensor *DayCumulatedEnergy_sensor = new Sensor();
+  Sensor *MonthCumulatedEnergy_sensor = new Sensor();
+  Sensor *YearCumulatedEnergy_sensor = new Sensor();
+  Sensor *TransmissionState_sensor = new Sensor();
+  Sensor *GlobalState = new Sensor();
 
   MyCustomSensor() : PollingComponent(15000) { }
 
-  void setup() override {
+  void setup() override 
+  {
     ESP_LOGD("Aurora", "Init setup()");
     Serial.begin(115200);
     ABBAurora::setup(Serial1, RX1, TX1, TX_CONTROL_GPIO);
-		inverter = new ABBAurora(INVERTER_ADDRESS);
-		ESP_LOGD("aurora", "Finish setup()");
+    inverter = new ABBAurora(INVERTER_ADDRESS);
+    ESP_LOGD("aurora", "Finish setup()");
   }
-  void update() override {
-    ESP_LOGD("Aurora", "Init update aurora");
-  	if (inverter->ReadDSPValue(POWER_IN_1, MODULE_MESSUREMENT))
-      {
-  		//String version = inverter->Version.Par1;
-      ESP_LOGD("aurora", "DSP.Value sensor is: %f", inverter->DSP.Value);
-  		DspValue_sensor->publish_state(inverter->DSP.Value);
-      }
 
-  	if (inverter->ReadCumulatedEnergy(CURRENT_DAY))
+	
+  void update() override 
+  {
+    ESP_LOGD("Aurora", "Init aurora update data");
+    if (inverter->ReadDSPValue(POWER_IN_1, MODULE_MESSUREMENT))
       {
-      ESP_LOGD("aurora", "CumulatedEnergy.Energy sensor is: %f", inverter->CumulatedEnergy.Energy);
-  		ReadCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+      	ESP_LOGD("Aurora", "DSP.Value sensor is: %f", inverter->DSP.Value);
+  	DspValue_sensor->publish_state(inverter->DSP.Value);
       }
+    if (inverter->ReadCumulatedEnergy(CURRENT_DAY))
+      {
+      	ESP_LOGD("Aurora", "CURRENT DAY Energy sensor is: %f", inverter->CumulatedEnergy.Energy);
+  	DayCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+      }	  
+    if (inverter->ReadCumulatedEnergy(CURRENT_MONTH))
+      {
+      	ESP_LOGD("Aurora", "CURRENT MONTH Energy sensor is: %f", inverter->CumulatedEnergy.Energy);
+  	MonthCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+      }
+    if (inverter->ReadCumulatedEnergy(CURRENT_YEAR))
+      {
+      	ESP_LOGD("Aurora", "CURRENT YEAR Energy sensor is: %f", inverter->CumulatedEnergy.Energy);
+  	YearCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);   
+      	ESP_LOGD("Aurora", "TransmissionState sensor is: %f", inverter->CumulatedEnergy.TransmissionState);
+  	TransmissionState_sensor->publish_state(inverter->CumulatedEnergy.TransmissionState);
+        ESP_LOGD("Aurora", "GlobalState sensor is: %f", inverter->CumulatedEnergy.GlobalState);
+  	GlobalState_sensor->publish_state(inverter->CumulatedEnergy.GlobalState);
+      }  	 
+  }
+	
+	
 
-    }
 };
